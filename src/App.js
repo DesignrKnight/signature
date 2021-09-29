b12603d49d1490f0c2ecd0cce5acf11366959317
Abel Mathew <notify.abelmathew@gmail.com>
2021-09-29 11:18:33 +0530

    initial commit (#3)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import LoginButton from './auth/LoginButton';
-import LogoutButton from './auth/LogoutButton';
-import Profile from './auth/Profile';
+import LoginButton from "./auth/LoginButton";
+import LogoutButton from "./auth/LogoutButton";
+import Profile from "./auth/Profile";
+import React from "react";
 
 function App() {
-	return (
-		<>
-			<LoginButton />
-			<LogoutButton />
-			<Profile />
-		</>
-	);
+  return (
+    <>
+      <LoginButton />
+      <LogoutButton />
+      <Profile />
+    </>
+  );
 }
 
 export default App;
