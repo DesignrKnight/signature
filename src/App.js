2761b41d356818e2b1fe25c79c3c12ed0127470a
Abel Mathew <notify.abelmathew@gmail.com>
2021-09-29 10:26:43 +0530

    Revert "initial commit (#1)" (#2)
    This reverts commit d08b5e5ad87f464af5d35e483a0c127884158f70.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import LoginButton from './auth/LoginButton';
+import LogoutButton from './auth/LogoutButton';
+import Profile from './auth/Profile';
+
+function App() {
+	return (
+		<>
+			<LoginButton />
+			<LogoutButton />
+			<Profile />
+		</>
+	);
+}
+
+export default App;
